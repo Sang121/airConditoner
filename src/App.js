12868fe31ac1610cb8c75ebf7df8eb89eca8abe4
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-06-01 18:34:40 +0700

    edit link

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
         </div>
       </body>
       <footer className="footer">
-      <p className="src"> Điều hoà created by <a href="https:www.fb.com/libra245"> Sáng,giúp bạn mát trong lòng:))</a></p> <br/>
+      <p className="src"> Điều hoà created by <a href="https:www.fb.com/libra245"> Sáng</a>,giúp bạn mát trong lòng:))</p> 
 
     </footer>
     </div>
